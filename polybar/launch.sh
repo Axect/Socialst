610072ef9722ffb4f400f52c69ef49cf69218cc9
Tae Geun, Kim <edeftg@gmail.com>
2018-10-22 14:21:25 +0900

    Polybar Multiple Monitor

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,6 +7,12 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar example
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
 
 echo "Bars launched..."
