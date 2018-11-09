bb41ff005b6b60d542ecc98935217e7d78b0e191
axect <edeftg@gmail.com>
2018-11-10 00:46:06 +0900

    Package

--- Script/package.sh
+++ Script/package.sh
@@ -6,4 +6,11 @@ yaourt -S vim uim\
   ghc mailspring xorg-xrandr arandr\
   r rstudio-desktop-bin jupyter-notebook\
   python-pip python-numpy python-scipy python-matplotlib\
-  npm zathura-ps zathura-pdf-mupdf zathura-djvu zathura-cb --noconfirm
+  npm zathura-ps zathura-pdf-mupdf zathura-djvu zathura-cb\
+  julia sagemath visual-studio-code-bin dlang --noconfirm
+
+# Rust
+curl https://sh.rustup.rs -sSf | sh
+
+# Nim
+curl https://nim-lang.org/choosenim/init.sh -sSf | sh
