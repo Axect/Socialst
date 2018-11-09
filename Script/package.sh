22888f37ec825501640104b6b67d55b9ceded2c5
axect <edeftg@gmail.com>
2018-11-10 01:58:46 +0900

    Good Openbox3

--- Script/package.sh
+++ Script/package.sh
@@ -1,13 +1,15 @@
-yaourt -S vim uim\
+yaourt -S uim\
   nerd-fonts-complete-mono-glyphs\
   texlive-most texlive-lang\
   tk gcc-fortran slack-desktop\
-  grive neofetch ttf-nanum okular\
-  ghc mailspring xorg-xrandr arandr\
+  grive ttf-nanum okular\
+  ghc mailspring\
   r rstudio-desktop-bin jupyter-notebook\
   python-pip python-numpy python-scipy python-matplotlib\
   npm zathura-ps zathura-pdf-mupdf zathura-djvu zathura-cb\
   julia sagemath visual-studio-code-bin dlang --noconfirm
+
+#  xorg-xrandr arandr\
 
 # Rust
 #curl https://sh.rustup.rs -sSf | sh
