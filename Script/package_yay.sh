1bb79faa7bec661cf3d1d88fe62b6dd54db6c8f3
axect <axect@outlook.kr>
2019-04-16 11:49:56 +0900

    Package update

--- Script/package_yay.sh
+++ Script/package_yay.sh
@@ -1,13 +1,16 @@
 # Official Repository
-sudo yay -S uim texlive-most texlive-lang tk gcc-fortran r jupyter-notebook\
+yay -S --noconfirm --needed uim texlive-most texlive-lang tk gcc-fortran r jupyter-notebook\
   python-pip python-numpy python-scipy python-matplotlib npm zathura-ps zathura-pdf-poppler zathura-djvu zathura-cb\
   julia sagemath dlang pandoc\
   zsh-history-substring-search zsh-autosuggestions\
-  cmus filezilla easytag youtube-dl sagemath-jupyter flameshot eog 
+  cmus filezilla easytag youtube-dl sagemath-jupyter flameshot eog\
+  lsb-release
   
 # AUR
-yay -S slack-desktop grive ttf-nanum mailspring\
-  rstudio-desktop-bin acroread
+yay -S --noconfirm --needed slack-desktop grive ttf-nanum \
+  acroread \
+  hiri \
+  insync
 
 # Rust
 curl https://sh.rustup.rs -sSf | sh
