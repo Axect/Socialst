7394375581554768c2411fd15f7c10cb0f2dd6d4
axect <axect@outlook.kr>
2019-04-27 13:13:04 +0900

    Fix error of Pylab

--- Templates/Pylab_Template/plot.py
+++ Templates/Pylab_Template/plot.py
@@ -7,7 +7,7 @@ with open("data.pickle", "rb") as fr:
 n = len(data)
 
 # Use latex
-plt.rc('text', usetext=True)
+plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
 
 # Plot
@@ -15,11 +15,11 @@ plt.figure(figsize=(10,6), dpi=300)
 plt.title(r"Title", fontsize=16)
 plt.xlabel(r'$x', fontsize=14)
 plt.ylabel(r'$y$', fontsize=14)
-plt.legend(fontsize=12)
-plt.grid()
 
 x = data[0]
 for i in range(1, n):
     plt.plot(x, data[i], label=r'$i='+str(i)+'$')
 
+plt.legend(fontsize=12)
+plt.grid()
 plt.savefig("plot.png", dpi=300)
