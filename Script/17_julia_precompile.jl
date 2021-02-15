69a1abb001aa19c298dc550b1756b32453322a82
Axect <axect@outlook.kr>
2021-02-16 01:57:05 +0900

    Add zygote

--- Script/17_julia_precompile.jl
+++ Script/17_julia_precompile.jl
@@ -1,6 +1,12 @@
 using PackageCompiler, IJulia
-using Flux, Plots, DifferentialEquations, JuMP
+using Flux, Plots, DifferentialEquations, JuMP, Zygote, BenchmarkTools
 
-create_sysimage([:Flux, :Plots, :DifferentialEquations, :JuMP], sysimage_path="/home/xteca/zlib/sys_fpdj.so")
+create_sysimage([:Flux,
+                 :Plots,
+                 :DifferentialEquations,
+                 :JuMP,
+                 :Zygote,
+                 :BenchmarkTools
+                ], sysimage_path="/home/xteca/zlib/sys_fpdj.so")
 
 IJulia.installkernel("Julia FPDJ", "--sysimage=/home/xteca/zlib/sys_fpdj.so")
