601b36da097704c77c30fee923e33925fc94ef22
Axect <axect@outlook.kr>
2021-02-15 12:27:15 +0900

    Coc Update

--- Script/07_coc.sh
+++ Script/07_coc.sh
@@ -1,1 +1,3 @@
-nvim -c 'CocInstall -sync coc-rust-analyzer coc-html coc-json coc-css coc-r-lsp coc-python coc-snippets coc-git coc-explorer coc-actions coc-clangd|q'
+nvim -c 'PlugInstall|q'
+
+nvim -c 'CocInstall -sync coc-rust-analyzer coc-html coc-json coc-css coc-r-lsp coc-python coc-snippets coc-git coc-explorer coc-actions coc-clangd coc-julia coc-vimtex|q'
