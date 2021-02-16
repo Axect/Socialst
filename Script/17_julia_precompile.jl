63f3597c13461434cd8a9e358e21bd69591a111b
Axect <axect@outlook.kr>
2021-02-16 19:14:29 +0900

    Add Julia package - NCDataFrame

--- Script/17_julia_precompile.jl
+++ Script/17_julia_precompile.jl
@@ -1,12 +1,14 @@
 using PackageCompiler, IJulia
-using Flux, Plots, DifferentialEquations, JuMP, Zygote, BenchmarkTools
+using Flux, Plots, DifferentialEquations, JuMP, Zygote, BenchmarkTools, NCDataFrame, DataFrames
 
 create_sysimage([:Flux,
                  :Plots,
                  :DifferentialEquations,
                  :JuMP,
                  :Zygote,
-                 :BenchmarkTools
+                 :BenchmarkTools,
+                 :NCDataFrame,
+                 :DataFrames
                 ], sysimage_path="/home/xteca/zlib/sys_fpdj.so")
 
 IJulia.installkernel("Julia FPDJ", "--sysimage=/home/xteca/zlib/sys_fpdj.so")
