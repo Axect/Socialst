1c43ce9863ce072c9a615a6c3433131dbd59cc6b
Axect <axect@outlook.kr>
2022-08-05 23:34:53 +0900

    Zellij & ncplot modify

--- Templates/PyPlot_Template/nc_plot.py
+++ Templates/PyPlot_Template/nc_plot.py
@@ -1,24 +1,24 @@
 from netCDF4 import Dataset
 import matplotlib.pyplot as plt
 
+# Use latex
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+
 # Import netCDF file
 ncfile = './data.nc'
 data = Dataset(ncfile)
 var = data.variables
 
-# Use latex
-plt.rc('text', usetex=True)
-plt.rc('font', family='serif')
+# Prepare Data to Plot
+x = var['x'][:]
+y = var['y'][:]  
 
 # Prepare Plot
 plt.figure(figsize=(10,6), dpi=300)
 plt.title(r"Title", fontsize=16)
 plt.xlabel(r'$x$', fontsize=14)
 plt.ylabel(r'$y$', fontsize=14)
-
-# Prepare Data to Plot
-x = var['x'][:]
-y = var['y'][:]  
 
 # Plot with Legends
 plt.plot(x, y, label=r'$y=x$')
