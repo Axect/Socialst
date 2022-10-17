710098f25d47fc76d9f90d4ff26bb4469a8216c5
axect <axect@outlook.kr>
2022-10-18 07:41:56 +0900

    Modify typora theme

--- Script/typora.sh
+++ Script/typora.sh
@@ -1,6 +1,6 @@
 export SOCIALST=$HOME/Socialst
 
 rm $HOME/.config/Typora/themes/whitey.css
-ln -s $SOCIALST/Typora/whitey.css $HOME/.config/Typora/themes/whitey.css
+ln -s $SOCIALST/Typora/whitey2.css $HOME/.config/Typora/themes/whitey.css
 mkdir $HOME/.config/Typora/themes/whitey
-cp $SOCIALST/Typora/*.woff $HOME/.config/Typora/themes/whitey
+cp $SOCIALST/Typora/*.otf $HOME/.config/Typora/themes/whitey
