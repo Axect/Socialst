8f4eab8a5942dea81794744443402e8265dce038
axect <axect.tg@proton.me>
2024-04-22 20:37:18 +0900

    Commentbox customize

--- NeoVim/socialst.lua
+++ NeoVim/socialst.lua
@@ -20,7 +20,18 @@ return {
   { "wakatime/vim-wakatime", lazy = false },
 
   -- Comment box
-  { "LudoPinelli/comment-box.nvim" },
+  { 
+    "LudoPinelli/comment-box.nvim",
+    config = function()
+      local keymap = vim.keymap.set
+      local opts = { noremap = true, silent = true }
+      local cb = require("comment-box")
+
+      keymap({ "n", "v" }, "<leader>cb", "<Cmd>CBllbox 20<CR>", opts)
+      keymap({ "n", "v" }, "<leader>cl", "<Cmd>CBlline<CR>", opts)
+      keymap({ "n", "v" }, "<leader>cd", "<Cmd>CBd<CR>", opts)
+    end
+  },
 
   -- Typst
   {
