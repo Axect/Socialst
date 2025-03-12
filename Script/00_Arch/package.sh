e93328c6b6b8a0c60a7acd7ab23e2dc5b98a84de
axect <axect.tg@proton.me>
2025-03-12 09:24:27 +0900

    Add times new roman

--- Script/00_Arch/package.sh
+++ Script/00_Arch/package.sh
@@ -34,7 +34,7 @@ yay -S --needed \
   tectonic kime \
   solaar \
   helix bash-language-server julia python-lsp-server rust-analyzer texlab \
-  ttf-ibm-plex yt-dlp python-scienceplots \
+  ttf-ibm-plex yt-dlp python-scienceplots ttf-times-new-roman \
   pyright eslint
 # sagemath-jupyter
 # pandoc-citeproc
